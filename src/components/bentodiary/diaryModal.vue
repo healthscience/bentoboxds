276fec8665e891c6063a1f43b7c6c865724c5d75
aboynejames <aboynejames@gmail.>
2024-06-19 19:49:15 +0100

    new diary store and display of test events WIP

--- src/components/bentodiary/diaryModal.vue
+++ src/components/bentodiary/diaryModal.vue
@@ -6,7 +6,7 @@
           <slot name="header">default header</slot>
         </div>
         <div class="modal-body">
-          <div id="ecdiary">Diary Please</div>
+          <div id="ecdiary">Diary Please {{ smartDiary }}</div>
           <slot name="body">
             
             default body
@@ -28,7 +28,10 @@
 </template>
 
 <script setup>
-  import { ref, nextTick, onMounted, onUpdated } from 'vue'
+import { ref, nextTick, computed, onMounted, onUpdated } from 'vue'
+import { diaryStore } from '@/stores/diaryStore.js'
+
+  const storeDiary = diaryStore()  
 
   const props = defineProps({
     show: Boolean
@@ -36,101 +39,24 @@
 
   let ec = ref({})
 
-  // const ecdiary = ref(null)
-  let calSettings = {}
+  onMounted(() => {
+    console.log('mounted')
+    storeDiary.createEvents()
+  })
 
   onUpdated(() => {
-    console.log('diary')
-    console.log(document.getElementById('ecdiary'))
-    calSettings = {
-      view: 'timeGridWeek',
-      headerToolbar: {
-          start: 'prev,next today',
-          center: 'title',
-          end: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek resourceTimeGridWeek,resourceTimelineWeek'
-      },
-      resources: [
-          {id: 1, title: 'Resource A'},
-          {id: 2, title: 'Resource B'}
-      ],
-      scrollTime: '09:00:00',
-      events: createEvents(),
-      views: {
-          timeGridWeek: {pointer: true},
-          resourceTimeGridWeek: {pointer: true},
-          resourceTimelineWeek: {
-              pointer: true,
-              slotMinTime: '09:00',
-              slotMaxTime: '21:00',
-              slotWidth: 80,
-              resources: [
-                  {id: 1, title: 'Resource A'},
-                  {id: 2, title: 'Resource B'},
-                  {id: 3, title: 'Resource C'},
-                  {id: 4, title: 'Resource D'},
-                  {id: 5, title: 'Resource E'},
-                  {id: 6, title: 'Resource F'},
-                  {id: 7, title: 'Resource G'},
-                  {id: 8, title: 'Resource H'},
-                  {id: 9, title: 'Resource I'},
-                  {id: 10, title: 'Resource J'},
-                  {id: 11, title: 'Resource K'},
-                  {id: 12, title: 'Resource L'},
-                  {id: 13, title: 'Resource M'},
-                  {id: 14, title: 'Resource N'},
-                  {id: 15, title: 'Resource O'}
-              ]
-          }
-      },
-      dayMaxEvents: true,
-      nowIndicator: true,
-      selectable: true
-  }
-  // ec = new EventCalendar(ecdiary, {}) 
-    nextTick(() => {
-      console.log('next tick called')
       let smartCal = document.getElementById('ecdiary')
-      console.log('smartc call222')
-      console.log(smartCal)
-      if (smartCal) {
-        ec = new EventCalendar(smartCal, calSettings)
-      }
-    })
-
+      ec = new EventCalendar(smartCal, eventDiary.value)
   })
 
   /* computed */
-    // settings for smart diary
-    const createEvents = () => {
-      let days = [];
-      for (let i = 0; i < 7; ++i) {
-          let day = new Date();
-          let diff = i - day.getDay();
-          day.setDate(day.getDate() + diff);
-          days[i] = day.getFullYear() + "-" + _pad(day.getMonth()+1) + "-" + _pad(day.getDate());
-      }
+  const eventDiary = computed(() => {
+    return storeDiary.eventList
+  })
 
-      return [
-          {start: days[0] + " 00:00", end: days[0] + " 09:00", resourceId: 1, display: "background"},
-          {start: days[1] + " 12:00", end: days[1] + " 14:00", resourceId: 2, display: "background"},
-          {start: days[2] + " 17:00", end: days[2] + " 24:00", resourceId: 1, display: "background"},
-          {start: days[0] + " 10:00", end: days[0] + " 14:00", resourceId: 1, title: "The calendar can display background and regular events", color: "#FE6B64"},
-          {start: days[1] + " 16:00", end: days[2] + " 08:00", resourceId: 2, title: "An event may span to another day", color: "#B29DD9"},
-          {start: days[2] + " 09:00", end: days[2] + " 13:00", resourceId: 2, title: "Events can be assigned to resources and the calendar has the resources view built-in", color: "#779ECB"},
-          {start: days[3] + " 14:00", end: days[3] + " 20:00", resourceId: 1, title: "", color: "#FE6B64"},
-          {start: days[3] + " 15:00", end: days[3] + " 18:00", resourceId: 1, title: "Overlapping events are positioned properly", color: "#779ECB"},
-          {start: days[5] + " 10:00", end: days[5] + " 16:00", resourceId: 2, title: {html: "You have complete control over the <i><b>display</b></i> of events…"}, color: "#779ECB"},
-          {start: days[5] + " 14:00", end: days[5] + " 19:00", resourceId: 2, title: "…and you can drag and drop the events!", color: "#FE6B64"},
-          {start: days[5] + " 18:00", end: days[5] + " 21:00", resourceId: 2, title: "", color: "#B29DD9"},
-          {start: days[1], end: days[3], resourceId: 1, title: "All-day events can be displayed at the top", color: "#B29DD9", allDay: true}
-        ];
-  }
-
-  const _pad = (num) => {
-      let norm = Math.floor(Math.abs(num));
-      return (norm < 10 ? '0' : '') + norm;
-  }
-
+  const smartDiary = computed(() => {
+    return storeDiary.diarySmart
+  })
 
 </script>
 
@@ -158,7 +84,7 @@
   border-radius: 2px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
   transition: all 0.3s ease;
-  overflow-y: scroll;
+  overflow: scroll;
 }
 
 .modal-header h3 {
